64689b22e7346e3f6f6850b6e0ba28e9c62ee09f
Dahleen, Luke <ladahle@ilstu.edu>
2018-05-09 12:20:30 -0500

    code formatting

--- background.js
+++ background.js
@@ -7,12 +7,10 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
   }, function(array_of_Tabs) {
       // Since there can only be one active tab in one active window,
       //  the array has only one element
-  	const numSentences = 9;
+  	  const numSentences = 9;
       var tab = array_of_Tabs[0];
       var url = "http://smmry.com/" + tab.url + "#&SM_LENGTH=" + numSentences;
-    	var newURL = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
       chrome.tabs.create({ url: url });
-      // ... do something with url variable
   });
   function httpGetAsync(theUrl, callback)
   {
@@ -21,7 +19,7 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
     }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.open("GET", theUrl, true);
     xmlHttp.send(null);
   }
     //chrome.tabs.create({ url: newURL });
