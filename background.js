460ddc2f7b4027707f0f9673b8b174a51d20d68e
Dahleen, Luke <ldahleen54@gmail.com>
2018-05-09 17:12:17 -0500

    error testing

--- background.js
+++ background.js
@@ -1,30 +1,42 @@
 
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
+chrome.browserAction.onClicked.addListener(function(activeTab){
+  alert("test");
   chrome.tabs.query({
       active: true,               // Select active tabs
       lastFocusedWindow: true     // In the current window
   },
-  function(array_of_Tabs) {
+  function(array_of_Tabs){
       // Since there can only be one active tab in one active window,
       //  the array has only one element
+      console.log("test");
   	  const numSentences = 9;
       var tab = array_of_Tabs[0];
       var url = "http://smmry.com/" + tab.url + "#&SM_LENGTH=" + numSentences;
-      httpGetAsync(url, function(responseText) {
-         console.log(responseText)
-      });
+      // var text = httpGet(url);
+      // alert(text);
       chrome.tabs.create({ url: url });
+
   });
-  function httpGetAsync(theUrl, callback)
-  {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true);
-    xmlHttp.send(null);
-  }
+  //
+  // function httpGet(theUrl){
+  //   var xmlHttp = new XMLHttpRequest();
+  //   xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+  //   xmlHttp.send( null );
+  //   return xmlHttp.responseText;
+  // }
+
+//   function httpGetAsync(theUrl, callback){
+//     // alert("test");
+//     var xmlHttp = new XMLHttpRequest();
+//     xmlHttp.onreadystatechange = function() {
+//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+//           alert("response");
+//           callback(xmlHttp.responseText);
+//         }
+//         // alert("response2");
+//
+//     }
+//     xmlHttp.open("GET", theUrl, true);
+//     xmlHttp.send(null);
+  // }
     //chrome.tabs.create({ url: newURL });
-});
