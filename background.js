c8796895c513fb904472ca3644f49f09ad38620f
Dahleen, Luke <ladahle@ilstu.edu>
2018-05-09 12:27:14 -0500

    added http request to console log

--- background.js
+++ background.js
@@ -4,12 +4,16 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
   chrome.tabs.query({
       active: true,               // Select active tabs
       lastFocusedWindow: true     // In the current window
-  }, function(array_of_Tabs) {
+  },
+  function(array_of_Tabs) {
       // Since there can only be one active tab in one active window,
       //  the array has only one element
   	  const numSentences = 9;
       var tab = array_of_Tabs[0];
       var url = "http://smmry.com/" + tab.url + "#&SM_LENGTH=" + numSentences;
+      httpGetAsync(url, function(responseText) {
+         console.log(responseText)
+      });
       chrome.tabs.create({ url: url });
   });
   function httpGetAsync(theUrl, callback)
